2be00bbab598e84ccb4df8760a6c9facf8c1dc07
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-04-04 02:41:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import re
+import os
+from os import getenv, environ
+
+emojis = ["👍", "🔥","👌", "🕊", "🤡", "😍", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡", "🏆", "💋", "👻", "👨‍💻", "👀",]
+api_id = int(environ.get("API_ID", ""))
+api_hash = environ.get("API_HASH", "")
+bot_token = environ.get("BOT_TOKEN", "")
