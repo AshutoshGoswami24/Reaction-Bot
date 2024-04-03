c41b302387686e8c2ca72af661f8f52aaaeb091b
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-04-04 02:44:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ emojis = ["👍", "🔥","👌", "🕊", "🤡", "😍", "❤‍🔥", "🌚", "
 api_id = int(environ.get("API_ID", ""))
 api_hash = environ.get("API_HASH", "")
 bot_token = environ.get("BOT_TOKEN", "")
+ASHUBHIRUNTHISBOT = True 
