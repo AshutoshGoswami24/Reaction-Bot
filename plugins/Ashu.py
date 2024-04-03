ec3741ace1bf40d972e02ebece347e8b24d0008d
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-04-04 02:43:37 +0530

    Create Ashu.py

---
+++ plugins/Ashu.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+import requests
+import random
+from config import *
+
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+@app.on_message()
+async def react_to_message(client, message):
+    chat_id = message.chat.id
+    message_id = message.id
+    
+    # Choose a random emoji from the list
+    random_emoji = random.choice(emojis)
+    
+    url = f'https://api.telegram.org/bot{bot_token}/setMessageReaction'
+
+    # Parameters for the request
+    params = {
+        'chat_id': chat_id,
+        'message_id': message_id,
+        'reaction': [{
+            "type": "emoji",
+            "emoji": random_emoji
+        }]
+    }
+
+    response = requests.post(url, json=params)
+
+    if response.status_code == 200:
+        print("Reaction set successfully!")
+        print("Response content:", response.content)
+    else:
+        print(f"Failed to set reaction. Status code: {response.status_code}")
+        print("Response content:", response.content)
