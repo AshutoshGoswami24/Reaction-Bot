8202abb1a0f46e82fa39868b1c3eeaaba815d986
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-05-04 09:35:07 +0530

    Update config.py
    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from os import getenv, environ
 
 emojis = ["👍", "🔥","👌", "🕊", "🤡", "😍", "❤‍🔥", "🌚", "🌭", "💯", "🤣", "⚡", "🏆", "💋", "👻", "👨‍💻", "👀",]
-api_id = int(environ.get("API_ID", ""))
-api_hash = environ.get("API_HASH", "")
-bot_token = environ.get("BOT_TOKEN", "")
+api_id = int(environ.get("25586552", ""))
+api_hash = environ.get("f265cba9d76dc6ad70914accbe758f47", "")
+bot_token = environ.get("7027209561:AAHFVReTuudSI6gZc9_75XpVXgijDHY6EM0", "")
 ASHUBHIRUNTHISBOT = True 
